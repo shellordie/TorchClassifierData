b1240c3bde6e11d620db0d6ac0f12c75e4388bd9
shellordie <shellordie@gmai.com>
2023-09-12 18:44:06 -1200

    update setup

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
 from setuptools import setup, find_packages
 
 VERSION = '0.0.1' 
-DESCRIPTION = "ok"
+DESCRIPTION = "Small pytorch utility to Import,Split,Normalize and Visualize custom dataset for classification tasks"
 
 setup(
         name="TorchClassifierData", 
         version=VERSION,
         author="charles TCHANAKE",
-        author_email="datadevfernolf@gmail.com",
-        #url="https://github.com/charleslf2/visionner",
+        author_email="shellordie@gmail.com",
+        url="https://github.com/shellordie/TorchClassifierData",
         description=DESCRIPTION,
        # long_description_content_type="text/markdown",
         #long_description=open("README.md","r",encoding="utf-8").read(),
         packages=find_packages(),
         install_requires=["numpy","opencv-python",
                           "matplotlib","pandas","torch"],  
-        #keywords=["Computer-vision",
-        #"preprocessing","Images","Dataset","visionner"],
-        #classifiers= [
-        #    "Development Status :: 3 - Alpha",
-        #    "Programming Language :: Python :: 3",
-        #    "Operating System :: MacOS :: MacOS X",
-        #    "Operating System :: Microsoft :: Windows",
-        #]
+        keywords=["Computer-vision","pytorch",
+        "preprocessing","Images","Dataset","TorchClassifierData"],
+        classifiers= [
+            "Development Status :: 3 - Alpha",
+            "Programming Language :: Python :: 3",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: Microsoft :: Windows",
+        ]
 )
